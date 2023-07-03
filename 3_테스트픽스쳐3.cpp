27e35083e13ecf23b16f6a9571cd38fa7ae2d4fc
Chansik Yun <chansigi@gmail.com>
2023-07-03 05:39:54 +0000

    .

--- 3_테스트픽스쳐3.cpp
+++ 3_테스트픽스쳐3.cpp
@@ -27,6 +27,16 @@ public:
 // 단점: 픽스쳐 설치 코드가 테스트 케이스 외부에 존재하기 때문에,
 //      테스트케이스만으로 테스트 코드를 분석하기 어려울 수 있다.
 
+// * xUnit Test Framework에서
+//   테스트 케이스를 구성하는 방법 => 4단계 테스트 패턴(Four Phase Test Pattern)
+// 1단계: 테스트 픽스쳐를 설치하거나, 실제 결과를 관찰하기 위해 필요한 것을 설정합니다. => SetUp()
+// 2단계: SUT와 상호작용 합니다. => TestBody()
+// 3단계: 기대 결과를 확인합니다. => TestBody()
+// 4단계: 테스트 픽스쳐를 해체해서, 테스트 시작 이전의 상태로 돌려 놓습니다. => TearDown()
+
+// * 테스트 스위트
+//  => 동일한 테스트 픽스쳐를 공유하는 테스트 케이스의 집합
+
 class CalcTest : public testing::Test {
 protected:
     Calc* calc = nullptr;
