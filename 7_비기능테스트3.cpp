a05873470c233ce59e1a62d9df7cc23e9576d70d
Chansik Yun <chansigi@gmail.com>
2023-07-04 01:32:50 +0000

    .

---
+++ 7_비기능테스트3.cpp
@@ -0,0 +1,70 @@
+// 7_비기능테스트2.cpp
+#include <iostream>
+#include <string>
+
+class Image {
+    std::string url;
+
+public:
+    Image(const std::string& s)
+        : url(s)
+    {
+    }
+
+    void Draw() const
+    {
+        std::cout << "Draw Image " << url << std::endl;
+    }
+
+    //-----
+    static int allocCount;
+    void* operator new(size_t size)
+    {
+        ++allocCount;
+        return malloc(size);
+    }
+
+    void operator delete(void* p, size_t)
+    {
+        --allocCount;
+        free(p);
+    }
+    //-----
+};
+
+int Image::allocCount = 0;
+
+bool DrawImage(const std::string& url)
+{
+    Image* image = new Image(url);
+    image->Draw();
+
+    // delete image;
+
+    return true;
+}
+
+#include <gtest/gtest.h>
+
+// 명시적인 TestSuite 클래스를 통해서
+// 누수의 검증을 수행합니다.
+class ImageTest : public testing::Test {
+    int alloc = 0;
+
+public:
+    void SetUp() override
+    {
+        alloc = Image::allocCount;
+    }
+
+    void TearDown() override
+    {
+        int diff = Image::allocCount - alloc;
+        EXPECT_EQ(diff, 0) << diff << " object(s) leaked";
+    }
+};
+
+TEST_F(ImageTest, DrawImage)
+{
+    EXPECT_TRUE(DrawImage("https://a.com/a.jpg"));
+}
