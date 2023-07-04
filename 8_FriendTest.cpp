a6df051f7d915e65b03eabc311ad96022f266c27
Chansik Yun <chansigi@gmail.com>
2023-07-04 01:55:42 +0000

    .

--- 8_FriendTest.cpp
+++ 8_FriendTest.cpp
@@ -18,8 +18,15 @@ public:
 // #undef private
 // #undef class
 // > 절대 좋은 방법이 아닙니다.
-
 #include <gtest/gtest.h>
+
+// * 제품 코드는 테스트를 작성하는 비용이 최소화되어야 합니다.
+// * private 메소드
+//  => public 메소드를 검증할 때, 자연스럽게 private 메소드도
+//     호출되어서 검증되어야 합니다.
+//  => private 메소드는 public 메소드의 가독성을 높이기 위해
+//     사용해야 합니다.
+//   "Clean Code"
 
 TEST(UserTest, foo)
 {
