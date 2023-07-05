39d9a3ce2d02e43f4f29b84481c328524708e4fb
Chansik Yun <chansigi@gmail.com>
2023-07-05 06:21:23 +0000

    .

--- 20_행위기반검증3.cpp
+++ 20_행위기반검증3.cpp
@@ -38,7 +38,12 @@ public:
 
 //  : EXPECT_CALL
 //    을 이용해서 delegating을 수행할 수 있습니다.
+//     .WillOnce       => 한번의 결과 제어
+//     .WillRepeatedly => 연속적인 결과 제어
+
 //    * 주의사항: 함수 호출 횟수 판단에 영향을 줍니다.
+//    => .WillOnce 갯수 => Times(N)
+//       .WillRepeatedly => AtLeast
 
 //   - EXPECT_CALL(..)               => Times(1)
 
@@ -49,6 +54,17 @@ public:
 //      .WillOnce(Return(30))
 //      .WillOnce(Return(30))
 
+//   - EXPECT_CALL(..)               => Times(AtLeast(2)) ; 2번 이상
+//      .WillOnce(Return(30))
+//      .WillOnce(Return(30))
+//      .WillRepeatedly(Return(42)); => AtLeast
+
+//  -  EXPECT_CALL(mock, Add(10, 20)) => Times(AtLeast(0))
+//      .WillRepeatedly(Return(42));
+//     => 무의미한 검증의 코드입니다. 절대 실패하지 않습니다.
+
+// * WillRepeatedly 가장 마지막에 한번만 사용이 가능합니다.
+
 using testing::Return;
 
 int Add(int a, int b) { return a + b; }
@@ -56,17 +72,39 @@ struct Adder {
     int operator()(int a, int b) const { return a + b; }
 };
 
+TEST(CalcTest, Sample3)
+{
+    MockCalc mock;
+
+    EXPECT_CALL(mock, Add(10, 20))
+        .WillRepeatedly(Return(42));
+
+    // std::cout << mock.Add(10, 20) << std::endl;
+    // std::cout << mock.Add(10, 20) << std::endl;
+    // std::cout << mock.Add(10, 20) << std::endl;
+    // std::cout << mock.Add(10, 20) << std::endl;
+}
+
+/*
 TEST(CalcTest, Sample2)
 {
     MockCalc mock;
 
     EXPECT_CALL(mock, Add(10, 20))
         .WillOnce(Return(30))
-        .WillOnce(Return(100));
+        .WillOnce(Return(100))
+        .WillRepeatedly(Return(42));
 
     std::cout << mock.Add(10, 20) << std::endl;
-    // std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
 }
+*/
 
 TEST(CalcTest, Sample)
 {
