af4f3136e062e11a3b389fbac4291069b4f41228
Chansik Yun <chansigi@gmail.com>
2023-07-05 00:19:45 +0000

    .

---
+++ 17_Mocking.cpp
@@ -0,0 +1,30 @@
+// 17_Mocking.cpp
+#include <string>
+#include <map>
+#include <utility>
+
+// 협력 객체의 인터페이스
+class MP3 {
+public:
+    virtual ~MP3() { }
+
+    virtual void Play() = 0;
+    virtual void Stop(int n) = 0;
+};
+
+#include <gmock/gmock.h>
+
+// 협력 객체의 인터페이스를 Mocking 합니다.
+class MockMP3 : public MP3 {
+public:
+    // MOCK_METHOD(반환 타입, 함수 이름, (인자 정보), (한정자 정보));
+
+    // void Play() override {}
+    MOCK_METHOD(void, Play, (), (override));
+    MOCK_METHOD(void, Stop, (int n), (override));
+};
+
+TEST(MP3Test, Sample)
+{
+    MockMP3 mock; // !!!
+}
